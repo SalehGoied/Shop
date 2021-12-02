ab346656269b4a466accc93806d1e79003edf340
Saleh Goied <86879991+SalehGoied@users.noreply.github.com>
2021-12-02 04:44:04 +0200

    Update models.py

--- website/models.py
+++ website/models.py
@@ -11,7 +11,7 @@ from sqlalchemy.sql.expression import false
 
 DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')  
 DB_USER = os.getenv('DB_USER', 'postgres')  
-DB_PASSWORD = os.getenv('DB_PASSWORD', '01558125032saleh')  
+DB_PASSWORD = os.getenv('DB_PASSWORD', '*****')  
 DB_NAME = os.getenv('DB_NAME', 'shop')  
 DB_PATH = 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
 
